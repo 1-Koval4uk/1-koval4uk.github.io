1a6b66f36a35f28502b5ad14e7997a423fa7c786
1-Koval4uk <46053952+1-Koval4uk@users.noreply.github.com>
2020-08-04 15:39:06 +0300

    Second commit

---
+++ assets/sites/new-prov/assets/js/main.js
@@ -0,0 +1,43 @@
+//Appearance animation
+new WOW().init();
+
+
+//Navigation scroll
+$(window).scroll(function(){
+    let wScroll = $(this).scrollTop();
+    if (wScroll > 20) {
+        $('.navigation').addClass('navigation-active');
+    }
+    else {
+        $('.navigation').removeClass('navigation-active');
+    };
+});
+
+
+$('.nav-list__hamburger').click(function(){
+    $('.left-side').removeClass('non-active')
+})
+
+
+$('.left-side__close').click(function () {
+    $('.left-side').addClass('non-active')
+});
+
+$(window).on('scroll', function(){
+    if($(window).scrollTop() >= 1000){
+        $('.btn-top').fadeIn();
+    }else{
+        $('.btn-top').fadeOut();
+    }
+})
+$('.btn-top').click(function(){
+    $('html, body').animate({scrollTop:0}, 800)
+})
+
+
+$('.testimonials-row__slider').slick({
+    dots: true,
+    autoplay: true,
+    speed: 900,
+    autoplaySpeed: 7500,
+});
